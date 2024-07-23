c56001ab6c46175664d8ebac4bc4258c2c0f3e35
jemnset <jemn.set@gmail.com>
2024-07-23 15:37:40 -0400

    Container size and grid size moved to javascript file so that they can be changed dynamically based on user input

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,14 +1,30 @@
+let containerSize = 800;
+let gridSize = 16;
+
 const container = document.querySelector("div");
 container.classList.add("container");
+container.style.width = containerSize + "px";
+container.style.height = containerSize + "px";
 
-let gridSize = 16 * 16;
+createGrid(gridSize);
 
-for(let i=0; i<gridSize; i++){
-    const item = document.createElement("div");
-    item.classList.add("item");
-    item.textContent = `${i % 16 +  1}`;
-    item.addEventListener("mouseover", ()=> {
-        item.style.background = "blue";
-    });
-    container.appendChild(item);
+
+function createGrid(gridSize){
+    
+    for(let i=0; i<(gridSize * gridSize); i++){
+        const item = document.createElement("div");
+        item.classList.add("item");
+        item.style.flex = `0 0 ${containerSize / gridSize}px`;
+        item.textContent = `${i % 16 +  1}`;
+        item.addEventListener("mouseover", ()=> {
+            item.style.background = "blue";
+        });
+        container.appendChild(item);
+    }
 }
+/** 
+const resetBtn = document.createElement("button");
+resetBtn.textContent = "Reset";
+resetBtn.addEventListener("click", ()=> {
+    
+});*/
