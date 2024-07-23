54973d436654313271a6cfec97865199912723de
jemnset <jemn.set@gmail.com>
2024-07-23 15:15:34 -0400

    Added event listner to change div colour on mouseover event

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -7,5 +7,8 @@ for(let i=0; i<gridSize; i++){
     const item = document.createElement("div");
     item.classList.add("item");
     item.textContent = `${i % 16 +  1}`;
+    item.addEventListener("mouseover", ()=> {
+        item.style.background = "blue";
+    });
     container.appendChild(item);
 }
