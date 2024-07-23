22f9ebb9ba6cdb975632c85c07d36bff421e555d
jemnset <jemn.set@gmail.com>
2024-07-23 16:14:46 -0400

    Added reset button functionality to reset the canvas with the user input grid size

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -8,23 +8,33 @@ container.style.height = containerSize + "px";
 
 createGrid(gridSize);
 
-
 function createGrid(gridSize){
     
     for(let i=0; i<(gridSize * gridSize); i++){
         const item = document.createElement("div");
         item.classList.add("item");
         item.style.flex = `0 0 ${containerSize / gridSize}px`;
-        item.textContent = `${i % 16 +  1}`;
         item.addEventListener("mouseover", ()=> {
             item.style.background = "blue";
         });
         container.appendChild(item);
     }
 }
-/** 
+
 const resetBtn = document.createElement("button");
 resetBtn.textContent = "Reset";
 resetBtn.addEventListener("click", ()=> {
+
+    const input = prompt("Please enter a new grid size (1-100):");
     
-});*/
+    if(!Number.isNaN(input) && Number.isInteger(Number(input)) && input >= 1 && input <= 100){
+        while(container.hasChildNodes()){
+            container.removeChild(container.firstChild);
+        }
+        createGrid(Number(input));
+    }else if(input != null){
+        alert("Invalid input. Unable to reset canvas.");
+    }
+});
+
+document.body.prepend(resetBtn);
